c3996157576fd6ad42e0b444c73349e2d9ae098e
NehaSijaria <nehasijaria@gmail.com>
2021-11-23 19:18:49 -0500

    finf if ur score is more than avg

---
+++ katas/betterThanAvg.js
@@ -0,0 +1,16 @@
+// Find if you're better than the average student in your class.
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!Return True if you're better, else False!
+//Note:Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
+function betterThanAverage(classPoints, yourPoints) {
+  // Your code here
+  var classAvg = 0;
+  for (var i = 0; i < classPoints.length; i++){
+    classAvg += classPoints[i]; 
+  }
+  classAvg = classAvg/classPoints.length; 
+  return yourPoints > classAvg;
+}
+
+const sol = betterThanAverage([2, 3], 5);
+console.log(sol);
